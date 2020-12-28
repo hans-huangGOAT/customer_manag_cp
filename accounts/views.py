6ce74012af6ec948aef5edc1dc8a52a01f7c3db3
Hans <fb_hans@outlook.com>
2020-12-29 07:55:09 +0800

    Frontend Stuff

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    context = {
+
+    }
+    return render(request, 'accounts/dashboard.html', context)
+
+
+def customer(request):
+    context = {
+
+    }
+    return render(request, 'accounts/customer.html', context)
+
+
+def products(request):
+    context = {
+
+    }
+    return render(request, 'accounts/products.html', context)
